22a784378628ea7b95e666bae1702cc528c5b00d
nivorako <nivo.rakoto@yahoo.fr>
2021-11-20 18:43:46 +0100

    Faire disparaitre modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector("#close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+//modalClose.foreach((elt) => elt.addEventListener("click", closeModal));
+modalClose.addEventListener("click", closeModal);
+
 // launch modal form
-function launchModal() {
+function launchModal() {  
   modalbg.style.display = "block";
 }
-
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
